4ef8c02f6fab9b64acabaf18be032cb9d5052337
ctspencer <62153750+ctspencer@users.noreply.github.com>
2020-03-25 15:35:10 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+
+import dash
+import dash_cytoscape as cyto
+import dash_html_components as html
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    cyto.Cytoscape(
+        id='cytoscape-nodes',
+        layout={'name': 'preset'},
+        style={'width': '100%', 'height': '1000px'},
+        elements=[
+            {'data': {'id': 'MThub', 'label': 'Major Topics'}, 'position': {'x': 225, 'y': 175}, 'grabbable': False},
+            {'data': {'id': 'MT1', 'label': 'Multidisciplinary Initiatives'}, 'position': {'x': 75, 'y': 75}},
+            {'data': {'id': 'MT2', 'label': 'Vulnerable Populations'}, 'position': {'x': 225, 'y': 275}},
+            {'data': {'id': 'MT3', 'label': 'Education & Awareness'}, 'position': {'x': 375, 'y': 75}},
+            {'data': {'source': 'MThub', 'target': 'MT1'}},
+            {'data': {'source': 'MThub', 'target': 'MT2'}},
+            {'data': {'source': 'MThub', 'target': 'MT3'}},
+        ]
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
